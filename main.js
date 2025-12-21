7527d05dd2543e1fff1b43ccac911a513e6b38c6
jenny <jenny.w0400@gmail.com>
2025-12-21 19:51:52 +0100

    WIP cleanup for demonstrationpurposes

--- main.js
+++ main.js
@@ -35,8 +35,8 @@ function draw() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 function addContent(_element) {
-    for (let i = 0; i < 80; i++) {
-        _element.innerHTML = _element.innerHTML + "hihi hoho jsjsjsjjshhbs bbhhaga lorem ipsum blablabla jhahhaha jjjjj lalalalal skksskkkw";
+    for (let i = 0; i < 3; i++) {
+        _element.innerHTML = _element.innerHTML + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
     }
 }
 function handleKeyInput(_event) {
--- main.ts
+++ main.ts
@@ -51,8 +51,8 @@ function draw(){
 }
 
 function addContent(_element:HTMLElement){
-    for(let i:number=0;i<80;i++){
-        _element.innerHTML=_element.innerHTML+"hihi hoho jsjsjsjjshhbs bbhhaga lorem ipsum blablabla jhahhaha jjjjj lalalalal skksskkkw"
+    for(let i:number=0;i<3;i++){
+        _element.innerHTML = _element.innerHTML +"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
     }
 }
 
